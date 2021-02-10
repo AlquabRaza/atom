ef7b4546e5b635a6a179d7d138d98e81dbde91c7
AlqaubRaza <alquab.20507@sscbs.du.ac.in>
2021-02-11 04:15:23 +0530

    nested looping challenge

---
+++ section9_challenge.cpp
@@ -0,0 +1,36 @@
+section 9
+Challenge
+/*
+  This challenge is about using a collection (list) of integers and allowing the user
+  to select options from a menu to perform operations on the list.
+
+  Your Program should display a menu to the user as follows:
+
+  P- Print Numbers
+  A- Add a Number
+  M- Display mean of the Numbers
+  S- Display the smallest Number
+  L- Display the largest Number
+  Q- Quit
+
+  Enter Your Choice
+
+The program should only accept valid choices from the user, both upper and lowercase selections should be allowed.
+If an illegal choice is made, you should display, "Unknown Selection, please try again" and the menu options should be
+displayed again.
+
+If the user enters 'P' or 'p' , you should display all of the element(ints) in the list.
+If the list is empty you should display "[] - the list is empty"
+If the list is not empty then all the list element should be displayed inside square brackets separated by a space.
+For example, [ 1 2 3 4 5 ]
+
+If the user enters 'A' or 'a' then you should prompt the user for an integer to add to the list
+which you will add to the list and then display it was added. For example, if the user enters 5
+You should display, '5 added'.
+Duplicate list entries are OK
+
+If user enters 'M' or 'm' you should calculate the mean of the elements in the list and display it.
+If the list is empty you should display. "Unable to find mean- no data"
+
+  and so on.....
+*/
